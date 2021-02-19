7e031396deddee1266450221afc7c1031115f5b3
QEDK <1994constant@gmail.com>
2021-02-19 16:44:57 +0530

    :new: Add form validation

--- swagur/augur/views.py
+++ swagur/augur/views.py
@@ -2,4 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-	return render(request, "augur/index.html")
+    return render(request, "augur/index.html")
+
+def user(request):
+    if request.method == "GET":
+        return HttpResponse("App is running")
+
+def market(request):
+    if request.method == "GET":
+        return HttpResponse("App is running")
