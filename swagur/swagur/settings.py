53c58efc8cad646b5220e7214be423f4d82bb76f
QEDK <1994constant@gmail.com>
2021-02-21 17:07:16 +0530

    :wrench: Add debug env

--- swagur/swagur/settings.py
+++ swagur/swagur/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = bool(os.environ.get("DEBUG", False))
 
 ALLOWED_HOSTS = ["localhost", "swagur.herokuapp.com"]
 
@@ -74,18 +74,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'swagur.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
