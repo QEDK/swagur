29eedb41514202aa86b2c6c6105fc5087454e32b
QEDK <1994constant@gmail.com>
2021-02-21 15:38:49 +0530

    :wrench: Remove debug in prod

--- swagur/swagur/settings.py
+++ swagur/swagur/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["localhost", "swagur.herokuapp.com"]
 
